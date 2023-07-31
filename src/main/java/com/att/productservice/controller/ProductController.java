993dc393251ddf0432bb8fcbfa4c88d103c5d1bd
capalatamnobleproglat <capalatam@nobleproglat.onmicrosoft.com>
2023-07-31 12:03:42 +0000

    Products microservice

---
+++ src/main/java/com/att/productservice/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.att.productservice.controller;
+
+import com.att.productservice.dto.ProductDto;
+import com.att.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductDto> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        return productService.createProduct(productDto);
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto getProductById(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ProductDto updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) throws ChangeSetPersister.NotFoundException {
+        return productService.updateProduct(id, productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
+        productService.deleteProduct(id);
+    }
+}
