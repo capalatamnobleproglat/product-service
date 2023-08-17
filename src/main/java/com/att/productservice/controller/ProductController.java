8997aca901de4cfd61f3605b8465a0151c1a8721
capalatamnobleproglat <capalatam@nobleproglat.onmicrosoft.com>
2023-08-17 05:47:04 +0000

    improvements are added

--- src/main/java/com/att/productservice/controller/ProductController.java
+++ src/main/java/com/att/productservice/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.att.productservice.controller;
 import com.att.productservice.dto.ProductDto;
 import com.att.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
